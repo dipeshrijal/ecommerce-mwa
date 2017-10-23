65b7c82e6bc6633fddf0136c8ab9baf44a6faa88
Dipesh Rijal <dipesh.rijal@gmail.com>
2017-10-23 16:36:07 -0500

    form component

--- admin/src/app/products/product-list/product-list.component.ts
+++ admin/src/app/products/product-list/product-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  numbers: number[] = [1,2,3,4,5,6,7,8,9,0,12,2,2,2,222,22];
+
   constructor() { }
 
   ngOnInit() {
